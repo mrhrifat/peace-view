bbd55494c2db46a3e2592680bf451ac4c25e99a0
mrhrifat <mrhrifat383759@gmail.com>
2021-12-26 22:05:32 +0600

    Create base structure of Root components to import all with best places

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import { Grid } from '@material-ui/core'
+import index from '../api'
 
 export class Root extends Component {
     render() {
         return (
-            <div>
-                Bismillahir Rahmanir Rahim
-            </div>
+            <Grid container spacing={16} justify='center'>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        
+                        <Grid item xs={12}>
+                            {/* TODO: Search Bar */}
+                        </Grid>
+
+                        <Grid item xs={8}>
+                            {/* TODO: Video */}
+                        </Grid>
+
+                        <Grid item xs={4}>
+                            {/* TODO: List */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
