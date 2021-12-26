f3eaacf571cf368dc2fbd186ffdef4dcd70a235b
mrhrifat <mrhrifat383759@gmail.com>
2021-12-27 01:46:11 +0600

    Create a list of searched videos

--- src/components/sub_components/VideoItem.jsx
+++ src/components/sub_components/VideoItem.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Grid, Paper, Typography } from '@material-ui/core'
 
-const VideoItem = () => {
+const VideoItem = ({ video }) => {
+    // console.log(video)
     return (
-        <div>
-
-        </div>
+        <Grid item xs={12}>
+            <Paper style={{ display: 'flex', alignItems: 'center' }}>
+                <img
+                    style={{ marginRight: '20px' }}
+                    alt='Thumbnail'
+                    src={video.snippet.thumbnails.medium.url}
+                />
+                <Typography variant='subtitle1'>
+                    <strong>{video.snippet.title}</strong>
+                </Typography>
+            </Paper>
+        </Grid>
     )
 }
 
