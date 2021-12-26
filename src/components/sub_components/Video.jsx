d34e98316eaaf4f805a4ecb0e595e8e5706d3521
mrhrifat <mrhrifat383759@gmail.com>
2021-12-27 01:01:45 +0600

    Create a embed for show video & make time toString & others

--- src/components/sub_components/Video.jsx
+++ src/components/sub_components/Video.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Paper, Typography } from '@material-ui/core'
 
-const Video = () => {
+const Video = ({ selectedVideo }) => {
+
+
+    if (!selectedVideo) return 'Loading...'
+
+    console.log(selectedVideo)
+    // console.log(selectedVideo.id.videoId)
+
+    const videoSrc = `${process.env.REACT_APP_VIDEO_EMBED_URL}/${selectedVideo.id.videoId}`
+
+    const time = new Date(selectedVideo.snippet.publishedAt).toDateString()
+
+
     return (
-        <div>
-            Video
-        </div>
+        <>
+        Hi
+        </>
     )
 }
 
