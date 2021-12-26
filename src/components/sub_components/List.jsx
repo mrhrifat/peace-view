3989d034f0c09848399e5ae7d73c284a5432eabb
mrhrifat <mrhrifat383759@gmail.com>
2021-12-27 02:00:42 +0600

    Create a container for better view

--- src/components/sub_components/List.jsx
+++ src/components/sub_components/List.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import VideoItem from './VideoItem'
 
-const List = ({ videos }) => {
-    const videoList = videos.map((video, id) => <VideoItem key={id} video={video} />)
-    return videoList
+const List = ({ videos, onVideoSelect }) => {
+    const videoList = videos.map((video, id) => <VideoItem
+        key={id}
+        video={video}
+        onVideoSelect={onVideoSelect}
+    />)
+    return (
+        <Grid container spacing={10}>
+            {videoList}
+        </Grid>
+    )
 }
 
 export default List
