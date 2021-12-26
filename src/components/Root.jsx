11c8056d9cd8abcb91075d249d19d137c6e2d292
mrhrifat <mrhrifat383759@gmail.com>
2021-12-26 22:19:20 +0600

    Set components on palces after import

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Grid } from '@material-ui/core'
+import { Search, Video, List } from './sub_components'
 import index from '../api'
 
 export class Root extends Component {
@@ -8,17 +9,20 @@ export class Root extends Component {
             <Grid container spacing={16} justify='center'>
                 <Grid item xs={12}>
                     <Grid container spacing={16}>
-                        
+
                         <Grid item xs={12}>
                             {/* TODO: Search Bar */}
+                            <Search />
                         </Grid>
 
                         <Grid item xs={8}>
                             {/* TODO: Video */}
+                            <Video />
                         </Grid>
 
                         <Grid item xs={4}>
                             {/* TODO: List */}
+                            <List />
                         </Grid>
                     </Grid>
                 </Grid>
