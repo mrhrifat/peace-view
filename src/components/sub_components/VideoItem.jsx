9f4e4f46a90c3d31934a423b1b0dcacb091891a4
mrhrifat <mrhrifat383759@gmail.com>
2021-12-27 02:12:38 +0600

    Add click evenet on list videos

--- src/components/sub_components/VideoItem.jsx
+++ src/components/sub_components/VideoItem.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Grid, Paper, Typography } from '@material-ui/core'
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
     // console.log(video)
     return (
-        <Grid item xs={12}>
-            <Paper style={{ display: 'flex', alignItems: 'center' }}>
+        <Grid item xs={10}>
+            <Paper
+                style={{ cursor: 'pointer' }}
+                onClick={() => onVideoSelect(video)}
+            >
                 <img
                     style={{ marginRight: '20px' }}
                     alt='Thumbnail'
                     src={video.snippet.thumbnails.medium.url}
                 />
-                <Typography variant='subtitle1'>
+                <Typography variant='subtitle1' style={{ margin: '10px 0' }}>
                     <strong>{video.snippet.title}</strong>
                 </Typography>
             </Paper>
