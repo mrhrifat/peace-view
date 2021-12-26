ca1338304f57f76bc1a3228721c2a340f8e1eef9
mrhrifat <mrhrifat383759@gmail.com>
2021-12-27 01:02:33 +0600

    Create Video show iframe & full view

--- src/components/sub_components/Video.jsx
+++ src/components/sub_components/Video.jsx
@@ -6,7 +6,7 @@ const Video = ({ selectedVideo }) => {
 
     if (!selectedVideo) return 'Loading...'
 
-    console.log(selectedVideo)
+    // console.log(selectedVideo)
     // console.log(selectedVideo.id.videoId)
 
     const videoSrc = `${process.env.REACT_APP_VIDEO_EMBED_URL}/${selectedVideo.id.videoId}`
@@ -16,7 +16,23 @@ const Video = ({ selectedVideo }) => {
 
     return (
         <>
-        Hi
+            <Paper elevation={6} style={{ height: '70%' }}>
+                <iframe
+                    frameBorder='0'
+                    height='100%'
+                    width='100%'
+                    title='Video Player'
+                    src={videoSrc}
+                />
+            </Paper>
+            <Paper elevation={6} style={{ padding: '15px' }}>
+                <Typography variant='h4'>
+                    {selectedVideo.snippet.title}
+                </Typography>
+                <Typography variant='subtitle1'>{videoSrc && time || 'No time available'}</Typography>
+                <Typography variant='h6'>{selectedVideo.snippet.channelTitle}</Typography>
+                <Typography variant='subtitle2'>{selectedVideo.snippet.description || 'No descreption available'} </Typography>
+            </Paper>
         </>
     )
 }
