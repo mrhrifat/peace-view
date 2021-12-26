5bc4e4866457030e3207e6c11729205b4b2fe98f
mrhrifat <mrhrifat383759@gmail.com>
2021-12-26 22:36:24 +0600

    Create handle change & submit & Form Text Filed

--- src/components/sub_components/Search.jsx
+++ src/components/sub_components/Search.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import { Paper, TextField } from '@material-ui/core'
+
 
 export class Search extends Component {
+
+    state = {
+        searchTerm: ''
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        const { searchTerm } = this.state
+        const { onFormSubmit } = this.props
+        onFormSubmit(searchTerm)
+    }
+
+    handleChange = e => this.setState({ searchTerm: e.target.value })
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <Paper elevation={6} style={{ padding: '25px' }}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField
+                        fullWidth
+                        label='Search'
+                        onChange={this.handleChange}
+                    />
+                </form>
+            </Paper>
         )
     }
 }
