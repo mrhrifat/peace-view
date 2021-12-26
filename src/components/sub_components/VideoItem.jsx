73e6dca1284bf9c35ae49aed6a771112b9c124a3
mrhrifat <mrhrifat383759@gmail.com>
2021-12-27 01:09:19 +0600

    Create functional video item component

---
+++ src/components/sub_components/VideoItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Grid, Paper, Typography } from '@material-ui/core'
+
+const VideoItem = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default VideoItem
