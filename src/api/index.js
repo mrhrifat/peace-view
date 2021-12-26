b0df7a0f0ac33d9debb49232ac7393c1a16f1673
mrhrifat <mrhrifat383759@gmail.com>
2021-12-26 23:20:15 +0600

    Remove parameter option beacuse it's not exist on new version

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: process.env.REACT_APP_API_KEY
-    }
+    baseURL: process.env.REACT_APP_BASE_URL
 })
