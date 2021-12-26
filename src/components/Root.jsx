6cce6f860164a8f9e0ec8c9ed642a1ffb9351415
mrhrifat <mrhrifat383759@gmail.com>
2021-12-26 21:43:22 +0600

    Convert Root.jsx functional component to class based component

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Root = () => {
-    return (
-        <div>
-            <h3>Bismillahir Rahmanir Rahim</h3>
-        </div>
-    )
+export class Root extends Component {
+    render() {
+        return (
+            <div>
+                Bismillahir Rahmanir Rahim
+            </div>
+        )
+    }
 }
 
 export default Root
