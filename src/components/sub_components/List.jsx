c43af97b639f9bc4df555228d5c41e7f8332b9c9
mrhrifat <mrhrifat383759@gmail.com>
2021-12-27 01:46:54 +0600

    Map all videos & return an individual video item

--- src/components/sub_components/List.jsx
+++ src/components/sub_components/List.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import VideoItem from './VideoItem'
 
-const List = () => {
-    return (
-        <div>
-            List
-        </div>
-    )
+const List = ({ videos }) => {
+    const videoList = videos.map((video, id) => <VideoItem key={id} video={video} />)
+    return videoList
 }
 
 export default List
